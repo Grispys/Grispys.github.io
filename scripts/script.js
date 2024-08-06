31b5a7e4e1aaccfe920f2ead3951fe63a1cf72c8
Grispys <grispanimation@gmail.com>
2024-08-06 16:29:14 -0230

    added corndog simulator and all of its functions

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+function ketchup(){
+    document.getElementById("corndog").src="./corndogImages/corndog_ketchup.png"
+}
+
+function mustard(){
+    document.getElementById("corndog").src="./corndogImages/corndog_mustard.png"
+}
+
+function hotdog(){
+    document.getElementById("corndog").src="./corndogImages/corndog_hotdog.png"
+}
+
+function donut(){
+    document.getElementById("corndog").src="./corndogImages/corndog_donut.png"
+}
+
+function website(){
+    document.getElementById("corndog").src="./corndogImages/corndog_website.png"
+}
+
+function chili(){
+    document.getElementById("corndog").src="./corndogImages/corndog_chili.png"
+}
+
+function chile(){
+    document.getElementById("corndog").src="./corndogImages/corndog_chile.png"
+}
+
+function pepper(){
+    document.getElementById("corndog").src="./corndogImages/corndog_pepper.png"
+}
+
+function milk(){
+    
+}
